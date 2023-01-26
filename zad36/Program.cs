2a0091756ed9da9e62e59965871f41b2f9362b68
kamran <askerari@mail.ru>
2023-01-26 04:01:47 +0300

    Решено 36я Задача

---
+++ zad36/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных индексах.
+
+
+int[] array = GetArray(9 , 0, 999);
+Console.WriteLine($"[{String.Join(",", array)}]");
+Console.WriteLine("Сумма элементов стоящих на нечетных = {SumElement(array)}");
+
+// Метод
+
+int[] GetArray(int size, int minValue, int maxValue){
+    int[] res = new int[size];
+    for(int i = 0; i < size; i++){
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+int SumElement(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+if (i % 2 == 1) sum += array[i];
+    }return sum;
+}
